11d2b1e9b8a6a80c3eb926341db191c79e567221
old4ever <git@old4ever.simplelogin.com>
2023-10-26 00:07:47 +0300

    34. Home & NotFound Components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 
 import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
-import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
 import User from "./components/users/User";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
 
@@ -22,17 +22,10 @@ const App = () => {
             <div className="container">
               <Alert />
               <Routes>
-                <Route
-                  path=""
-                  element={
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  }
-                />
+                <Route path="" Component={Home} />
                 <Route path="about" Component={About} />
                 <Route path={`/user/:login`} Component={User} />
+                <Route path="*" Component={NotFound} />
               </Routes>
             </div>
           </div>
